01536e82430495d3a78e7fe190888f5fdc2f1711
FahadwebD <fahadchowdhury66779@gmail.com>
2022-09-03 21:13:13 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 
 import './App.css';
+import Navbar from './Shared/Navbar';
 
 function App() {
   
     return (
-      <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-  
-  <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-  <p >
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
-</div>
+     <Navbar></Navbar>
     );
 }
 
