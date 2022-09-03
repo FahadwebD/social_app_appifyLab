48803c7f7b0f3b35d84349998f30ebdd1d5a4ccd
FahadwebD <fahadchowdhury66779@gmail.com>
2022-09-03 21:27:07 +0600

    basic layout created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Layout from './Layout/Layout';
 import Navbar from './Shared/Navbar';
 
 function App() {
   
     return (
+     <>
      <Navbar></Navbar>
+     <Layout></Layout>
+     </>
     );
 }
 
---
+++ src/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div className='container mx-auto mt-2'>
+            <div class="grid grid-cols-4 gap-4">
+  <div class="bg-black">01</div>
+  <div class=" col-span-2 bg-black">02</div>
+  <div class="bg-black">03</div>
+  
+</div>
+        </div>
+    );
+};
+
+export default Layout;
